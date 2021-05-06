bb1c5ca2d54a88caaec6b9feee96d7fccbab2e25
oc04 <dsoc04@gmail.com>
2021-05-06 17:41:35 +0900

    CHAPTER 12.3 공통 컴포넌트를 재사용하기(침실 개수, 침대개수) [완] BOOK P372 , pdf P50

---
+++ components/room/register/RegisterRoomBedrooms.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import styled from "styled-components";
+import palette from "../../../styles//palette";
+import { useDispatch } from "react-redux";
+import { useSelector } from "../../../store";
+import { registerRoomActions } from "../../../store/registerRoom";
+import { getNumber } from "../../../lib/utils";
+import { bedroomCountList } from "../../../lib/staticData";
+import Selector from "../../common/Selector";
+import Counter from "../../common/Counter";
+
+const Container = styled.div`
+    padding: 62px 30px 100px;
+    
+    h2 {
+        font-size: 19px;
+        font-weight: 800;
+        margin-bottom: 56px;
+        }
+    h3 {
+        font-weight: bold;
+        color: ${palette.gray_76};
+        margin-bottom: 6px;
+    }
+
+    .register-room-step-info {
+        font-size: 14px;
+        max-width: 400px;
+        margin-bottom: 24px;
+        max-width: 400px;
+        word-break: keep-all;
+    }
+
+    .register-room-mamximum-guest-count-wrapper{
+        width: 320px;
+        margin-top: 24px;
+        margin-bottom: 32px;
+    }
+
+    .register-room-bedroom-count-wrapper{
+        width: 320px;
+        margin-bottom: 32px;
+    }
+
+    .register-room-bed-count-wrapper{
+        width: 320px;
+        margin-bottom: 57px;
+    }
+`;
+
+const RegisterRoomBedrooms: React.FC = () => {
+    const maximumGuestCount = useSelector( (state) => state.registerRoom.maximumGuestCount);
+    const bedroomCount = useSelector( (state) => state.registerRoom.bedroomCount);
+    const bedCount = useSelector( (state) => state.registerRoom.bedCount);
+
+    const dispatch = useDispatch();
+
+    //* 최대 숙박 인원 변경 시
+    const onChangeMaximumGuestCount = (value: number) => {
+        dispatch(registerRoomActions.setMaximumGuestCount(value));
+    };
+
+    //* 침실 개수 변경 시
+    const onChangeBedroomCount = (event: React.ChangeEvent<HTMLSelectElement>) =>{
+        dispatch(registerRoomActions.seetBedroomCount(getNumber(event.target.value) || 0));
+    };
+
+    //* 침대 개수 변경 시
+    const onChangeBedCount = (value: number) =>
+        dispatch(registerRoomActions.setBedCount(value));
+
+    return (
+        <Container>
+            <h2>숙소에 얼마나 많은 인원이 숙박할 수 있나요?</h2>
+            <h3>2단계</h3>
+            <p className="room-register-step-info">
+                모든 게스트가 편안하게 숙박할 수 있도록 침대가 충분히 구비되어 있는지 확인하세요.
+            </p>
+            <div className="register-room-mamximum-guest-count-wrapper">
+                <Counter
+                    label="최대 숙박 인원"
+                    value={maximumGuestCount}
+                    onChange={onChangeMaximumGuestCount}
+                />
+            </div>
+            <div className="register-room-bedroom-count-wrapper">
+                <Selector
+                    type="register"
+                    value={`침실 ${bedroomCount}개`}
+                    onChange={onChangeBedroomCount}
+                    label="게스트가 사용할 수 있는 침실은 몇 개인가요?"
+                    options={bedroomCountList}
+                />
+            </div>
+            <div className="register-room-bed-count-wrapper">
+                <Counter label="침대" value={bedCount} onChange={onChangeBedCount} />
+            </div>
+        </Container>
+    );   
+};
+
+export default RegisterRoomBedrooms;
--- store/registerRoom.ts
+++ store/registerRoom.ts
@@ -1,10 +1,17 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { BedType } from "../types/room";
 
 type RegisterRoomState = {
     largeBuildingType: string | null;
     buildingType: string | null;
     roomType: string | null;
     isSetUpForGuest: boolean | null;
+
+    maximumGuestCount: number;
+    bedroomCount: number;
+    bedCount: number;
+    bedList: {id: number; beds:{ type: BedType; count: number }[] }[];
+    publicBedList: { type: BedType; count: number }[];
 };
 
 //* 초기상태
@@ -17,6 +24,17 @@ const initialState: RegisterRoomState ={
     roomType: null,
     //* 게스트만을 위해 만들어진 숙소인가
     isSetUpForGuest: null,
+
+    //* 최대 숙박 인원
+    maximumGuestCount: 1,
+    //* 침실 개수
+    bedroomCount: 0,
+    //* 침대 개수
+    bedCount: 1,
+    //* 침대 유형
+    bedList: [],
+    //* 공용공간 침대 유형
+    publicBedList: [],
 };
 
 const registerRoom = createSlice({
@@ -52,6 +70,25 @@ const registerRoom = createSlice({
             state.isSetUpForGuest = action.payload;
             return state;
         },
+
+        //* 최대 숙박 인원 변경하기
+        setMaximumGuestCount(state, action: PayloadAction<number>){
+            state.maximumGuestCount = action.payload;
+            return state;
+        },
+
+        //* 침실 개수 변경하기
+        seetBedroomCount(state, action: PayloadAction<number>){
+            state.bedroomCount = action.payload;
+            return state;
+        },
+
+        //* 최대 침대 개수 변경하기
+        setBedCount(state, action: PayloadAction<number>){
+            state.bedCount = action.payload;
+            return state;
+        },
+        
     },
 });
 
