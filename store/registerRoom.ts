4b437d992a2e802d92e34d76cf3e1b094c31cfaa
DESKTOP-J06RRVI\oc04 <dsoc04@gmail.com>
2021-05-23 13:56:14 +0900

    CHAPTER 15.1 숙소 등록하기 5단계 리덕스 설정  [완] BOOK P443 , pdf P121

--- store/registerRoom.ts
+++ store/registerRoom.ts
@@ -25,6 +25,8 @@ type RegisterRoomState = {
     latitude: number;
     longitude: number;
 
+    amentities: string[];
+
 };
 
 //* 초기상태
@@ -70,6 +72,9 @@ const initialState: RegisterRoomState ={
     latitude: 0,
     //* 경도
     longitude: 0,
+
+    //* 편의 시설
+    amentities: [],
 
 };
 
@@ -241,6 +246,12 @@ const registerRoom = createSlice({
         //* 경도 변경하기
         setLongitude(state, action: PayloadAction<number>) {
             state.longitude = action.payload;
+        },
+
+
+        //* 편의 시설 변경하기
+        setAmentities(state, action: PayloadAction<string[]>){
+            state.amentities = action.payload;
         },
       
     },
